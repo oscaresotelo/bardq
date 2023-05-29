31ed36a1db4277e2f5a65ac9775c26322200ac12
oscarsotelo <oscaresotelo@gmail.com>
2023-05-29 13:04:04 -0300

    primercomitdellunes

--- bard.py
+++ bard.py
@@ -1,80 +1,40 @@
-# import streamlit as st
-# from Bard import Chatbot
-
-# # Paste your Bard Token (check README.md for where to find yours) 
-# token = "Wghlci3JMlUaLO4_VZgMKjCDDhUSMRvL4049frEMZZaj3PK1cJ6iMDTJbZoeClmdcGaRYg."
-# # Initialize Google Bard API
-# chatbot = Chatbot(token)
-
-# def prompt_bard(prompt):
-#     response = chatbot.ask(prompt)
-#     return response['content']
-
-# def main():
-#     st.title("Preguntame")
-#     st.write("Ingrese sus Preguntas")
-
-#     prompt_text = st.text_input("Pregunta")
-#     if st.button("Preguntar"):
-#         # If prompt is empty, show error message
-#         if len(prompt_text.strip()) == 0:
-#             st.error("Por Favor Ingresar Pregunta")
-#         else:
-#             # Prompt Bard
-#             response = prompt_bard(prompt_text)
-#             st.write('Respuesta:', response)
-
-# if __name__ == '__main__':
-#     main()
 import streamlit as st
-from translate import Translator
-
 from Bard import Chatbot
+from textblob import TextBlob
 
-# Pegue su token de Bard (consulte el archivo README.md para saber dónde encontrar el suyo)
+import warnings
+import sys
+
+# Paste your Bard Token (check README.md for where to find yours) 
 token = "Wghlci3JMlUaLO4_VZgMKjCDDhUSMRvL4049frEMZZaj3PK1cJ6iMDTJbZoeClmdcGaRYg."
-# Inicializar el Chatbot de Bard
+# Initialize Google Bard API
 chatbot = Chatbot(token)
 
-# Inicializar el traductor
-def translate_to_spanish(text):
-    translator = Translator(to_lang="es", from_lang="en")
-    translation = translator.translate(text)
-    return translation
-
 def translate_to_english(text):
-    translator = Translator(to_lang="en", from_lang="es")
-    translation = translator.translate(text)
-    return translation
+    blob = TextBlob(text)
+    return str(blob.translate(from_lang= "es",to='en'))
+
+def translate_to_spanish(text):
+    blob = TextBlob(text)
+    return str(blob.translate(from_lang="en",to='es'))
 
 def prompt_bard(prompt):
-    response = chatbot.ask(prompt)
-    return response['content']
-
-def split_into_segments(text, segment_length):
-    segments = []
-    for i in range(0, len(text), segment_length):
-        segment = text[i:i + segment_length]
-        segments.append(segment)
-    return segments
+    prompt_english = translate_to_english(prompt)
+    response = chatbot.ask(prompt_english)
+    response_spanish = translate_to_spanish(response['content'])
+    return response_spanish
 
 def main():
     st.title("Pregúntame")
-    st.write("Ingrese sus Preguntas")
-
-    prompt_text = st.text_area("Pregunta")
+    prompt_text = st.text_input("Escribir:")
     if st.button("Preguntar"):
-        # Si la pregunta está vacía, mostrar mensaje de error
         if len(prompt_text.strip()) == 0:
-            st.error("Por favor ingrese una pregunta")
+            st.warning("Vacio, Pregunte Nuevamente.")
         else:
-            # Preguntar a Bard
-            prompt_text = translate_to_english(prompt_text)
             response = prompt_bard(prompt_text)
-            segments = split_into_segments(response, 400)
-            translated_segments = [translate_to_spanish(segment) for segment in segments]
-            translated_response = ''.join(translated_segments)
-            st.write('Respuesta:', translated_response)
+            
+            st.write(response)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
--- traducir.py
+++ traducir.py
@@ -1,9 +1,40 @@
-from translate import Translator
+import streamlit as st
+from Bard import Chatbot
+from textblob import TextBlob
+
+import warnings
+import sys
+
+# Paste your Bard Token (check README.md for where to find yours) 
+token = "Wghlci3JMlUaLO4_VZgMKjCDDhUSMRvL4049frEMZZaj3PK1cJ6iMDTJbZoeClmdcGaRYg."
+# Initialize Google Bard API
+chatbot = Chatbot(token)
 
 def translate_to_english(text):
-    translator = Translator(to_lang="en", from_lang="es")
-    translation = translator.translate(text)
-    return translation
+    blob = TextBlob(text)
+    return str(blob.translate(from_lang= "es",to='en'))
 
-texto = "cuando nacio Belgrano"
-print(translate_to_english(texto))
+def translate_to_spanish(text):
+    blob = TextBlob(text)
+    return str(blob.translate(from_lang="en",to='es'))
+
+def prompt_bard(prompt):
+    prompt_english = translate_to_english(prompt)
+    response = chatbot.ask(prompt_english)
+    response_spanish = translate_to_spanish(response['content'])
+    return response_spanish
+
+def main():
+    st.title("Pregúntame")
+    prompt_text = st.text_input("Escribir:")
+    if st.button("Preguntar"):
+        if len(prompt_text.strip()) == 0:
+            st.warning("Vacio, Pregunte Nuevamente.")
+        else:
+            response = prompt_bard(prompt_text)
+            
+            st.write(response)
+
+if __name__ == "__main__":
+    main()
+
