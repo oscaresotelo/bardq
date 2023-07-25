d510f6ee7bfa791367d72ecf38238018b61d8d0f
oscarsotelo <oscaresotelo@gmail.com>
2023-07-25 15:14:16 -0300

    ultimadelmartes

--- imagenapp.py
+++ imagenapp.py
@@ -1,9 +1,19 @@
 
 from bardapi import Bard
 import streamlit as st
-
+from gtts import gTTS
+import tempfile
+import os 
 token = 'YwhlcoKaK4On2dndWfbCoX9MifhxeC_5mVs6d6WAHBlGs3J88otYTjI0NKeHYH4R90VSrQ.'
 bard = Bard(token=token)
+
+def text_to_speech(text):
+	st.write(text)
+	tts = gTTS(text=text, lang="es", tld='us')
+	temp_file = tempfile.NamedTemporaryFile(delete=False)
+	temp_file.close()
+	tts.save(temp_file.name)
+	return temp_file.name
 
 def main():
     st.title("Ai - Cito")
@@ -22,7 +32,11 @@ def main():
         else:
             with st.spinner("Procesando..."):
                 bard_answer = bard.ask_about_image("summarize the text in spanish", image_data)
-                st.write(bard_answer['content'])
-
+                respuesta = bard_answer['content']
+                st.write(respuesta)
+                # audio_file = bard.speech(respuesta)
+                audio_file = text_to_speech(respuesta)
+                st.audio(audio_file, format='audio/mp3')
+                os.remove(audio_file)
 if __name__ == "__main__":
     main()
