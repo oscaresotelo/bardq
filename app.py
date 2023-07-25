cef9110531c7de7ba634d1d6409c3f21c85b1f2e
oscarsotelo <oscaresotelo@gmail.com>
2023-07-25 09:03:25 -0300

    ver

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from bardapi import Bard
 import os
 
-os.environ['_BARD_API_KEY'] = "YwhlcoKaK4On2dndWfbCoX9MifhxeC_5mVs6d6WAHBlGs3J88otYTjI0NKeHYH4R90VSrQ."
+import streamlit as st 
+
+st.title("PAGINA DE PRUEBA")
+
+# os.environ['_BARD_API_KEY'] = "YwhlcoKaK4On2dndWfbCoX9MifhxeC_5mVs6d6WAHBlGs3J88otYTjI0NKeHYH4R90VSrQ."
 
 
-print(Bard().get_answer("import streamlit as st, crear formulario para dar de alta empleados")['content'])
+# print(Bard().get_answer("import streamlit as st, crear formulario para dar de alta empleados")['content'])
